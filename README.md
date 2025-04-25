49150b3ebb95c52a7819656a788e3dc16e0aa045
ninthcircle <mainlakshayhoon@gmail.com>
2025-04-25 20:39:55 +0530

    Made lodge.h standalone

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 ---
 
 ## Features
+
 1. **Multiple Log Levels**: Supports `DEBUG`, `INFO`, `WARNING`, `ERROR`, and `FATAL` levels.
 2. **Log to File**: Option to output logs to a file in addition to standard output.
 3. **Timestamping**: Each log entry includes a timestamp to track when events occur.
@@ -9,23 +10,31 @@
 5. **Dynamic Log Level Control**: Set the log level at runtime to control the verbosity.
 
 ## Getting Started
+
 ### Requirements
+
 1. A C compiler (`GCC`, `Clang`, etc.)
 2. POSIX-compliant environment (for `pthread` support)
+
 ### Installation
+
 1. Include `lodge.h` into your project.
 2. Compile the project with `libpthread`.
+
 ```bash
-gcc your_program.c -o your_program -lpthread
+gcc your_program.c -o your_program -lpthread -DTHREAD_SAFE
 ```
 
 ## Usage
+
 1. Include the library.
+
 ```c
 #include "lodge.h"
 ```
 
 2. Initialize the logging system with an optional file path. If a file path is provided, logs will be saved to the file.
+
 ```c
 lodge_constructor(filepath); // Logs to a file
 // or
@@ -33,11 +42,13 @@ lodge_constructor(NULL); // Logs to stdout
 ```
 
 3. *(Optional)* Set desired log level to control verbosity.
+
 ```c
 lodge_set_log_level(LOG_DEBUG); // Options: LOG_DEBUG, LOG_INFO, LOG_WARNING, LOG_ERROR, LOG_FATAL
 ```
 
 4. Use the provided macros to log messages at different levels:
+
 ```c
 lodge_info("This is an informational message.");
 lodge_debug("This is a debug message with value: %d", 42);
@@ -52,6 +63,7 @@ lodge_fatal("Fatal error, exiting...");
 5. *(Important)* Before exiting your application, call `lodge_destructor()` to close any open file handles and free resources.
 
 ## Example
+
 ```c
 #include "lodge.h"
 
@@ -68,6 +80,7 @@ int main() {
     lodge_destructor();
     return 0;
 }
+
 ```
 ```console
 2024-11-02 13:55:34 [INFO]: Application started
--- lodge.h
+++ lodge.h
@@ -1,33 +1,82 @@
+// MIT License
+//
+// Copyright (c) 2025 Lakshay Chauhan
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Available at <https://raw.githubusercontent.com/nos1dot618/lodge/refs/heads/master/lodge.h>
+
+/* Example Usage
+
+#include "lodge.h"
+
+int main() {
+  lodge_constructor(NULL);
+  // NOTE: This will log to stdout, log to a file by passing the filepath in
+  //       the lodge_constructor.
+  lodge_set_log_level(LOG_DEBUG);
+  // NOTE: Disable logging of timestamp by calling lodge_toggle_timestamp API.
+  // lodge_toggle_timestamp();
+  lodge_info("Application started");
+  lodge_debug("Debugging value: %d", 100);
+  lodge_warning("This is a warning message.");
+  lodge_error("This is an error message.");
+  lodge_fatal("This is a fatal error message.");
+  lodge_destructor();
+  return 0;
+}
+
+*/
+
 #ifndef LODGE_H_
 #define LODGE_H_
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <time.h>
+
+#ifdef THREAD_SAFE
 #include <pthread.h>
+static pthread_mutex_t mutex;
+#endif
 
 typedef enum {
-    LOG_DEBUG,
-    LOG_INFO,
-    LOG_WARNING,
-    LOG_ERROR,
-    LOG_FATAL
+  LOG_DEBUG,
+  LOG_INFO,
+  LOG_WARNING,
+  LOG_ERROR,
+  LOG_FATAL
 } LogLevel;
 
 static const LogLevel default_log_level = LOG_INFO;
 static LogLevel current_log_level = default_log_level;
 static FILE *log_filepath = NULL;
-static pthread_mutex_t mutex;
 static int log_timestamp = 1;
 
-#define lodge_assert(condition, fmt, ...)                                  \
-    do {                                                                   \
-        if (!(condition)) {                                                \
-            fprintf(stderr, "Assertion Failed: "fmt"\n", ##__VA_ARGS__);   \
-            exit(EXIT_FAILURE);                                            \
-        }                                                                  \
-    } while (0)
+#define lodge_assert(condition, fmt, ...)                                      \
+  do {                                                                         \
+    if (!(condition)) {                                                        \
+      fprintf(stderr, "Assertion Failed: " fmt "\n", ##__VA_ARGS__);           \
+      exit(EXIT_FAILURE);                                                      \
+    }                                                                          \
+  } while (0)
 
 static const char *get_log_level_str(LogLevel level);
 void lodge_constructor(const char *filepath);
@@ -38,104 +87,92 @@ void lodge_toggle_timestamp();
 void lodge_destructor();
 
 static const char *get_log_level_str(LogLevel level) {
-    switch (level) {
-    case LOG_DEBUG:
-        return "DEBUG";
-    case LOG_INFO:
-        return "INFO";
-    case LOG_WARNING:
-        return "WARNING";
-    case LOG_ERROR:
-        return "ERROR";
-    case LOG_FATAL:
-        return "FATAL";
-    default:
-        lodge_assert(0, "unexpected log level '%d'", level);
-    }
+  switch (level) {
+  case LOG_DEBUG:
+    return "DEBUG";
+  case LOG_INFO:
+    return "INFO";
+  case LOG_WARNING:
+    return "WARNING";
+  case LOG_ERROR:
+    return "ERROR";
+  case LOG_FATAL:
+    return "FATAL";
+  default:
+    lodge_assert(0, "unexpected log level '%d'", level);
+  }
 }
 
 void lodge_constructor(const char *filepath) {
-    if (filepath) {
-        log_filepath = fopen(filepath, "a");
-        lodge_assert(log_filepath, "failed to open log file");
-    }
-    pthread_mutex_init(&mutex, NULL);
+  if (filepath) {
+    log_filepath = fopen(filepath, "a");
+    lodge_assert(log_filepath, "failed to open log file");
+  }
+#ifdef THREAD_SAFE
+  pthread_mutex_init(&mutex, NULL);
+#endif
 }
 
-void lodge_set_log_level(LogLevel level) {
-    current_log_level = level;
-}
+void lodge_set_log_level(LogLevel level) { current_log_level = level; }
 
 const char *lodge_get_current_timestamp() {
-    static char buffer[20];
-    time_t now = time(NULL);
-    struct tm *tm_info = localtime(&now);
-    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", tm_info);
-    return buffer;
+  static char buffer[20];
+  time_t now = time(NULL);
+  struct tm *tm_info = localtime(&now);
+  strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", tm_info);
+  return buffer;
 }
 
 void lodge_log_message(LogLevel level, const char *fmt, ...) {
-    // Skip logs below current log level
-    if (level < current_log_level) {
-        return;
-    }
-
-    // Lock mutex for thread safety
-    pthread_mutex_lock(&mutex);
-
-    const char *timestamp = lodge_get_current_timestamp();
-    const char *log_level_str = get_log_level_str(level);
-    if (log_filepath) {
-		if (log_timestamp) {
-			fprintf(log_filepath, "%s ", timestamp);
-		}
-        fprintf(log_filepath, "[%s]: ", log_level_str);
-        va_list args;
-        va_start(args, fmt);
-        vfprintf(log_filepath, fmt, args);
-        va_end(args);
-        fprintf(log_filepath, "\n");
-        fflush(log_filepath);
-    } else {
-		if (log_timestamp) {
-			fprintf(stdout, "%s ", timestamp);
-		}
-        fprintf(stdout, "[%s]: ", log_level_str);
-        va_list args;
-        va_start(args, fmt);
-        vfprintf(stdout, fmt, args);
-        va_end(args);
-        fprintf(stdout, "\n");
-    }
-
-    // Unlock mutex
-    pthread_mutex_unlock(&mutex);
+  if (level < current_log_level) {
+    return;
+  }
+#ifdef THREAD_SAFE
+  pthread_mutex_lock(&mutex);
+#endif
+  FILE *output = log_filepath ? log_filepath : stdout;
+  if (log_timestamp) {
+    fprintf(output, "%s ", lodge_get_current_timestamp());
+  }
+  fprintf(output, "[%s]: ", get_log_level_str(level));
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(output, fmt, args);
+  va_end(args);
+  fprintf(output, "\n");
+  if (log_filepath) {
+    fflush(log_filepath);
+  }
+#ifdef THREAD_SAFE
+  pthread_mutex_unlock(&mutex);
+#endif
 }
 
 #define lodge_debug(fmt, ...) lodge_log_message(LOG_DEBUG, fmt, ##__VA_ARGS__)
 #define lodge_info(fmt, ...) lodge_log_message(LOG_INFO, fmt, ##__VA_ARGS__)
-#define lodge_warning(fmt, ...) lodge_log_message(LOG_WARNING, fmt, ##__VA_ARGS__)
+#define lodge_warning(fmt, ...)                                                \
+  lodge_log_message(LOG_WARNING, fmt, ##__VA_ARGS__)
 #define lodge_error(fmt, ...) lodge_log_message(LOG_ERROR, fmt, ##__VA_ARGS__)
-// NOTE: calling lodge_destructor() in log_fatal may not be correct in thread context.
-// As this will close the file and destroy the mutex which is common between threads,
-// but calling fatal will only stop a single thread not the entire program.
-#define lodge_fatal(fmt, ...)                                  \
-    do {                                                       \
-        lodge_log_message(LOG_FATAL, fmt, ##__VA_ARGS__);      \
-        lodge_destructor();                                    \
-        exit(EXIT_FAILURE);                                    \
-    } while (0)
+// NOTE: calling lodge_destructor() in log_fatal may not be correct in thread
+//       context. As this will close the file and destroy the mutex which is
+//       common between threads, but calling fatal will only stop a single
+//       thread not the entire program.
+#define lodge_fatal(fmt, ...)                                                  \
+  do {                                                                         \
+    lodge_log_message(LOG_FATAL, fmt, ##__VA_ARGS__);                          \
+    lodge_destructor();                                                        \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
 
-void lodge_toggle_timestamp() {
-	log_timestamp = !log_timestamp;
-}
+void lodge_toggle_timestamp() { log_timestamp = !log_timestamp; }
 
-// Clean up logging resources
 void lodge_destructor() {
-    if (log_filepath) {
-        fclose(log_filepath);
-    }
-    pthread_mutex_destroy(&mutex);
+  if (log_filepath) {
+    fclose(log_filepath);
+  }
+#ifdef THREAD_SAFE
+  pthread_mutex_destroy(&mutex);
+#endif
 }
 
 #endif // LODGE_H_
